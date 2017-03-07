1b5e4982d0c75d08e937d700138cfc362c71e737
Carlton Bostick <acoachable1@gmail.com>
2017-03-07 01:13:05 -0600

    My first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any)
+    
+    {
+       // theLabel.text = "Hello There!!"  //replaces the label to this when button tapped
+       // print("Button tapped")  //displays in the operational output, not on the screen
+       tapCount = tapCount + 1
+        print(tapCount)
+        if tapCount >= 10 {
+            theLabel.text = "You tapped 10 times"
+        }
+    }
+    @IBAction func button2Tapped(_ sender: Any)
+    {
+        theLabel.text = "Buttons Are Cool!!"
+        print("Button 2 tapped") //displays in the operational output, not on the screen
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //theLabel.text = "Hello There!!"
     }
 
     override func didReceiveMemoryWarning() {
